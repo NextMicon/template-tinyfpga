6162629548c9f739f95ca394ac4b33ed81aecc0e
kanade-k-1228 <kanade.k.1228@gmail.com>
2023-09-07 14:19:50 +0900

    change dir

--- src/.common/ROM_CFG.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "reg.hpp"
-#include <stdint.h>
-
-class ROM_CFG : Reg {
-public:
-  ROM_CFG(volatile uint32_t* addr) : Reg(addr) {}
-  void dual_io() { Reg::set((Reg::get() & ~0x007F0000) | 0x00400000); };
-};
