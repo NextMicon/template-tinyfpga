7b00f0dbf7635f96ba178a9407d9f18e957ac273
kanade-k-1228 <kanade.k.1228@gmail.com>
2023-09-08 15:10:15 +0900

    change dir

--- src/simulation/serial.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/sh
-
-grep 'Serial data: .*' $1 \
-| sed 's/Serial data: //' \
-| sed "s/ 10/'\\\n'/" \
-| sed "s/'//" \
-| perl -pe "s/'\n//" \
-| perl -pe "s/\\\n/\n/g" \
-> $2
--- src/simulation/zeropadding.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-HEAD_PADDING=327680
-FOOT_PADDING=$((720896 - $(wc -c $1 | awk '{print $1}')))
-
-head -c $HEAD_PADDING /dev/zero > head.bin
-head -c $FOOT_PADDING /dev/zero > foot.bin
-cat head.bin $1 foot.bin
-rm head.bin foot.bin
